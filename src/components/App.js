084639efef947df446e50947aea1f44e6a84ecf0
dcornelius4 <d_cornelius94@yahoo.com>
2019-05-21 22:24:00 -0700

    added blue, green won't compile for some reason

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,19 @@ import {
   Route
 } from 'react-router-dom';
 import Red from './Red';
+import Blue from './Blue';
+import Green from '/Green';
 import Header from './Header';
 
-export default function Aoo() {
+export default function App() {
   return (
     <Router>
       <>
       <Header />
       <Switch>
         <Route path="/red" component={Red} />
+        <Route path="/blue" component={Blue} />
+        <Route path="/green" component={Green} />
       </Switch>
       </>
     </Router>
